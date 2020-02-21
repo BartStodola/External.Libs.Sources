c2262c8244e7ffe0c4d9e73b2faa7be9c2395c1c
chris.stadler <stadler.christian@web.de>
2020-02-21 11:29:55 +0100

    initial commit

---
+++ Logger/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Logger
+{
+    public interface ILogger
+    {
+        void Info(string txt);
+        void Warn(string txt);
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace Logger
+{
+    public class Logger : ILogger
+    {
+        public void Info(string txt)
+        {
+            System.Console.WriteLine("[info]"+txt);
+        }
+
+        public void Warn(string txt)
+        {
+            System.Console.WriteLine("[warn]" + txt);
+        }
+    }
+}
